d8a191a4c510f119b680fc4ed5565efa73799c6d
Cauã Carrijo Gomes Silva <kg364141@gmail.com>
2025-10-08 19:27:54 -0300

    Mudança no código db

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () => {
     try {
-        const { MONGO_URI } = process.env;
-        if (!MONGO_URI) throw new Error("MONGO_URI não está definido nas variáveis de ambiente.");
-        
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI) 
         console.log("MONGODB CONECTADO: ", conn.connection.host)
     } catch (error) {
         console.error("Erro de conexão no MONGODB : ", error)

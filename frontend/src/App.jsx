d5fa168175d28e09787157f52ee4690d566ecb1a
Cauã Carrijo Gomes Silva <kg364141@gmail.com>
2025-10-14 01:36:02 -0300

    otimização no código base do frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <div className="min-h-screen bg-slate-900 relative flex items-center justify-center p-4 overflow-hidden">
       {/* DECORATORS - GRID BG & GLOW SHAPES */}
       <div className="absolute inset-0 bg-[#0a0a0a] bg-[linear-gradient(to_right,#2e2e2e40_1px,transparent_1px),linear-gradient(to_bottom,#2e2e2e40_1px,transparent_1px)] bg-[size:13px_25px]" />
-      <div className="absolute top-0 -left-4 size-96 bg-white-500 opacity-20 blur-[100px]" />
+      <div className="absolute top-0 -left-4 size-96 bg-blue-500 opacity-20 blur-[100px]" />
       <div className="absolute bottom-0 -right-4 size-96 bg-neutral-400 opacity-20 blur-[100px]" />
 
       <button onClick={login} className="z-10">Login</button>
